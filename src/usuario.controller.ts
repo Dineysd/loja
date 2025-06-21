5b435faacd879727040410f3d91c1479437e5efe
Dineysd <dineysd@hotmail.com>
2025-06-21 12:53:42 -0300

    feat: criando endpoit usuario

--- src/usuario.controller.ts
+++ src/usuario.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 
 @Controller('/usuarios')
 export class UsuarioController{
     @Post()
-    async criarUsuario(){
-        return {status: 'usuario criado!'};
+    async criarUsuario(@Body() dadosDoUsuario){
+        return dadosDoUsuario;
     }
 
 }
